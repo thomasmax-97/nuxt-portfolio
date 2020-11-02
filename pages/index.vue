95e1be5d9339a5e0cb37d0e727446b3507b74384
thomasmax-97 <thompsenlp@googlemail.com>
2020-11-02 17:46:11 +0100

    landing-page: worked on index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,26 @@
 <template>
-  <div class="mt-navigation">
-    <nav class="flex items-center justify-between flex-wrap p-6 mt-primary-color sm:flex">
-      <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54"
-             xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/>
-        </svg>
-        <span class="font-semibold text-xl tracking-tight">Maximilian Thomas</span>
+  <div class="container-fluid mx-full w-full mt-work">
+    <div class="flex flex-col w-1/4 text-center mx-auto">
+      <div class="flex justify-center pt-8 pb-8">
+        <img class="rounded-full h-64 w-64" src="~/assets/maximilian-thomas.JPG"/>
       </div>
-      <div class="block lg:hidden">
-        <button
-          class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
-          </svg>
-        </button>
-      </div>
-      <div class="w-full block flex-grow lg:flex lg:items-center hidden md:visible">
-        <div class="text-sm lg:flex-grow">
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-            Home
-          </a>
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-            Referenzen
-          </a>
-          <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-            Über mich
-          </a>
-        </div>
-      </div>
-    </nav>
+      <span class="font-semibold tracking-tight text-white text-2xl">Maximilian Thomas</span>
+    </div>
   </div>
 </template>
-
 
 <script>
 export default {}
 </script>
 
 <style>
-.mt-primary-color {
-  background-color: #007fed
+.mt-work {
+  background-image: url('~assets/maximilian-thomas-work.jpg');
 }
+
+.mt-heading {
+  color: white;
+}
+
+
 </style>

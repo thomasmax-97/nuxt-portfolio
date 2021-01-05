c883b64be24a1116f3305879418e573725287dcb
thomasmax-97 <thompsenlp@googlemail.com>
2021-01-05 02:00:32 +0100

    feature/video: added bg video

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,9 +16,12 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
+  plugins: [{
+    src: '~/plugins/vue-background-video',
+    ssr: false
+  }],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
+// Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
